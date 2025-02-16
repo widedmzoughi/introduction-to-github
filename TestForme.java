d197d945670ad807102aab04e8d33719c0c11a8c
widedmzoughi <widedmzoughi89@gmail.com>
2025-02-16 18:59:24 +0100

    Create TestForme.java

---
+++ TestForme.java
@@ -0,0 +1,7 @@
+public class TestForme {
+    public static void main(String[] args) {
+        Cercle cercle = new Cercle(5);
+        cercle.drow();
+        cercle.colorer(0); // colorer en noir (0) le cercle
+    }
+}
